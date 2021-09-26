5bc7da3f9f16821b0d8248017f5da308703d8b0d
oyama <kamome2655@gmail.com>
2021-09-26 14:00:34 +0900

    kadai_r

--- kadai1_3_4.py
+++ kadai1_3_4.py
@@ -19,26 +19,40 @@
 
 #課題1-4 --------------------------------------------
 
-open_file = open('list.csv')
-raw_data = open_file.read()
-open_file.close()
+# def search():
+#     word = input("鬼滅の登場人物の名前を入力してください >>>")
+#     open_file = open('list.csv')
+#     raw_data = open_file.read()
+#     open_file.close()
+#     if word in raw_data:
+#         print("{}が見つかりました".format(word))
+#     if word not in raw_data:
+#         print("{}が見つかりませんでした".format(word))
+#         append_file = open("list.csv","a")
+#         append_file.write(word)
+#         append_file.close()
+#         open_file = open('list.csv')
+#         raw_data = open_file.read()
+#         open_file.close()
+#         print(raw_data)
+
+# search()
+
 
 def search():
     word = input("鬼滅の登場人物の名前を入力してください >>>")
-    if word in raw_data:
+    with open("list.csv", mode="r") as f:
+        source = f.read()
+    if word in source:
         print("{}が見つかりました".format(word))
-    if word not in raw_data:
+    if word not in source:
         print("{}が見つかりませんでした".format(word))
-        append_file = open("list.csv","a")
-        append_file.write(word)
-        append_file.close()
+        with open("list.csv", mode="a") as f:
+            f.write(word)
+        with open("list.csv", mode="r") as f:
+            source = f.read()
+            print(source)
 
 search()
-
-open_file = open('list.csv')
-raw_data = open_file.read()
-open_file.close()
-
-print(raw_data)
 
 
